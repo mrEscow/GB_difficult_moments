008c072e3d1b2b9db5255d467c4b6bfca3a8a1b9
mr.Escow <escowxbox@gmail.com>
2021-11-19 07:48:29 +0300

    test text

--- GB_difficult_moments_of_C++/main.cpp
+++ GB_difficult_moments_of_C++/main.cpp
@@ -3,9 +3,15 @@
 * * * * * * * * * */
 #pragma execution_character_set("utf-8")
 
+#include <Windows.h>
+
 #include "Lesson_1.h"
 
 int main() {
+
+	SetConsoleOutputCP(8);
+	SetConsoleCP(8);
+	// Тест
 
 	Lesson1();
 	
