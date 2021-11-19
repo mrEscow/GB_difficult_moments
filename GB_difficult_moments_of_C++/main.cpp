eb37558109e3e5ffd382c7f4c073934378f8c88f
mr.Escow <escowxbox@gmail.com>
2021-11-19 08:05:43 +0300

    Update Text

--- GB_difficult_moments_of_C++/main.cpp
+++ GB_difficult_moments_of_C++/main.cpp
@@ -3,15 +3,9 @@
 * * * * * * * * * */
 #pragma execution_character_set("utf-8")
 
-#include <Windows.h>
-
 #include "Lesson_1.h"
 
 int main() {
-
-	SetConsoleCP(CP_UTF8);
-	SetConsoleOutputCP(CP_UTF8);
-	// привет привет
 
 	Lesson1();
 	
